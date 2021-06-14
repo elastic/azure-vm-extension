d410d7fe40e25fcdd3469728ba6b8712ab94bb18
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-06-14 17:19:18 +0100

    Only run on daily basis if master branch

--- .ci/its.groovy
+++ .ci/its.groovy
@@ -42,7 +42,7 @@ pipeline {
     disableConcurrentBuilds()
   }
   triggers {
-    cron('H H(5-6) * * 1-5')
+    cron("${(env.BRANCH_NAME.trim() == 'master') 'H H(5-6) * * 1-5' ? ''}")
   }
   parameters {
     booleanParam(name: 'skipDestroy', defaultValue: "false", description: "Whether to skip the destroy of the cluster and terraform.")
