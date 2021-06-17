34f3fd56f855b14b06e0a027394c95363c6d0c83
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-06-17 11:24:32 +0100

    Move from waiting to sleep and while

--- test/ats/validate.py
+++ test/ats/validate.py
@@ -2,7 +2,6 @@ from typing import Counter
 import unittest
 import xmlrunner
 from elasticsearch import Elasticsearch
-from waiting import wait
 import os
 import time
 
@@ -17,95 +16,97 @@ class TestIndices(unittest.TestCase):
     ## TODO: Read from env variable
     isWindows = True
 
-    def countEnrolment(self, index_name, hostname):
-        records_count = self.es.count(index=index_name,body={
-                                "query": {
-                                    "bool": {
-                                        "filter": [
-                                            {
-                                                "match_all": {}
-                                            },
-                                            {
-                                                "match_phrase": {
-                                                    "local_metadata.host.hostname": hostname
-                                                }
-                                            },
-                                            {
-                                                "match_phrase": {
-                                                    "active": True
-                                                }
+    def countEnrolment(self, index_name, hostname, compareWith):
+        tries = 0
+        count = 0
+        while count < compareWith:
+            if tries > 20:
+                break
+            records_count = self.es.count(index=index_name,body={
+                            "query": {
+                                "bool": {
+                                    "filter": [
+                                        {
+                                            "match_all": {}
+                                        },
+                                        {
+                                            "match_phrase": {
+                                                "local_metadata.host.hostname": hostname
                                             }
-                                        ],
-                                        "must_not": [
-                                            {
-                                                "match_phrase": {
-                                                    "policy_id": "policy-elastic-agent-on-cloud"
-                                                }
+                                        },
+                                        {
+                                            "match_phrase": {
+                                                "active": True
                                             }
-                                        ]
-                                    }
+                                        }
+                                    ],
+                                    "must_not": [
+                                        {
+                                            "match_phrase": {
+                                                "policy_id": "policy-elastic-agent-on-cloud"
+                                            }
+                                        }
+                                    ]
                                 }
                             }
-                        )
+                        }
+                    )
+            count = records_count['count']
+            tries += 1
+            time.sleep(5)
+            continue
         ## Print will be shown in the junit xml as system-out. This should help to debug if needed.
         print(records_count)
-        return records_count['count']
+        return count
 
-    def count(self, index_name, hostname):
-        records_count = self.es.count(index=index_name, body={"query": {"match": {"agent.hostname": hostname}}})
+    def count(self, index_name, hostname, compareWith):
+        tries = 0
+        count = 0
+        while count < compareWith:
+            if tries > 20:
+                break
+            records_count = self.es.count(index=index_name, body={"query": {"match": {"agent.hostname": hostname}}})
+            count = records_count['count']
+            tries += 1
+            time.sleep(5)
+            continue
+
         ## Print will be shown in the junit xml as system-out. This should help to debug if needed.
         print(records_count)
-        return records_count['count']
+        return count
 
-    def waitForCount(self, index_name, hostname, compare):
-        count = 0
-        while count < compare:
-            count = self.count(index_name, hostname)
-            time.sleep(5)
-        return count >= compare
-
-    def waitForCountEnrolment(self, index_name, hostname, compare):
-        count = 0
-        while count < compare:
-            count = self.countEnrolment(index_name, hostname)
-            time.sleep(5)
-        return count >= compare
-
-    def waitForIndexExist(self, index_name):
+    def exists(self, index_name):
+        tries = 0
         exist = False
-        while not self.es.indices.exists(index_name):
+        while not exist:
+            if tries > 10:
+                break
+            ## Deprecated to access system indices
+            ## https://github.com/elastic/elasticsearch/issues/50251
+            exist = self.es.indices.exists(index_name)
+            tries += 1
             time.sleep(5)
-        return True
+            continue
+        return exist
 
-    def countAndTest(self, index_name, hostname, compare):
-        ## Let's wait a bit until the indices are ready
-        wait(lambda: self.waitForCount(index_name, self.hostname, compare), timeout_seconds=30, waiting_for="Index to be ready")
-        records_count = self.count(index_name, hostname)
-        self.assertTrue(records_count >= compare, "Expected at least one entry in index {}, got {}".format(index_name, records_count))
+    def countAndTest(self, index_name, hostname, compareWith):
+        records_count = self.count(index_name, hostname, compareWith)
+        self.assertTrue(records_count >= compareWith, "Expected at least one entry in index {}, got {}".format(index_name, records_count))
 
     def test_green_indices(self):
         records_indices = self.es.cat.indices()
         ## Print will be shown in the junit xml as system-out. This should help to debug if needed.
         print(records_indices)
-        self.assertTrue("green" in records_indices)
+        self.assertTrue("green" in records_indices, "Expected green indices")
 
     def test_indice_fleet_agents_7_exists(self):
-        index_name = '.fleet-agents-7'
-        ## Let's wait a bit until the indices are ready
-        wait(lambda: self.waitForIndexExist(index_name), timeout_seconds=120, waiting_for="Index to be ready")
-
-        ## Deprecated to access system indices
-        ## https://github.com/elastic/elasticsearch/issues/50251
-        self.assertTrue(self.es.indices.exists(index_name))
+        self.assertTrue(self.exists('.fleet-agents-7'), "Expected .fleet-agents-7 index")
 
     def test_enrolment(self):
         index_name = '.fleet-agents-7'
-        compare = 1
-        ## Let's wait a bit until the indices are ready
-        wait(lambda: self.waitForCountEnrolment(index_name, self.hostname, compare), timeout_seconds=120, waiting_for="Index to be ready")
-
-        records_count = self.countEnrolment(index_name, self.hostname)
-        self.assertTrue(records_count >= compare, "Expected at least one entry in index {}, got {}".format(index_name, records_count))
+        compareWith = 1
+        records_count = self.countEnrolment(index_name, self.hostname, compareWith)
+        self.assertTrue(records_count >= compareWith, "Expected at least one entry in index {}, got {}".format(index_name, records_count))
 
     def test_indice_ds_metrics_memory(self):
         self.countAndTest('.ds-metrics-system.memory-default-*', self.hostname, 1)
