c1acd382231115c76bfa60777ef9d426b75785e3
Maurizio Branca <maurizio.branca@gmail.com>
2023-06-27 07:56:26 +0200

    Clean up commented code

--- src/handler/windows/scripts/newconfig.ps1
+++ src/handler/windows/scripts/newconfig.ps1
@@ -21,71 +21,6 @@ function Uninstall-Old-ElasticAgent {
     while (-not $completed) {
         Try
         {
-            # $powershellVersion = Get-PowershellVersion
-            # $kibanaUrl = Get-Prev-Kibana-URL $powershellVersion
-            # if (-Not $kibanaUrl)
-            # {
-            #     throw "Kibana url could not be found"
-            # }
-            # Write-Log "Found Kibana url $kibanaUrl" "INFO"
-            # $password = Get-Prev-Password $powershellVersion
-            # $base64Auth = Get-Prev-Base64Auth $powershellVersion
-            # if (-Not $password -And -Not $base64Auth)
-            # {
-            #     throw "Password or base64auto key could not be found"
-            # }
-            # $agentId = Get-Agent-Id "$INSTALL_LOCATION\Elastic\Agent\fleet.yml"
-            # if (-Not $agentId)
-            # {
-            #     throw "Agent Id could not be found"
-            # }
-            # Write-Log "Unenroll elastic agent" "INFO"
-            # $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-            # $headers.Add("kbn-xsrf", "true")
-            # #cred
-            # $encodedCredentials = ""
-            # if ($password)
-            # {
-            #     $username = Get-Prev-Username $powershellVersion
-            #     if (-Not $username)
-            #     {
-            #         throw "Username could not be found"
-            #     }
-            #     $pair = "$( $username ):$( $password )"
-            #     $encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
-            # }
-            # else
-            # {
-            #     $encodedCredentials = $base64Auth
-            # }
-            # $headers.Add('Authorization', "Basic $encodedCredentials")
-            # $body = (@{ 'force' = $true } | ConvertTo-Json)
-            # $OLD_STACK_VERSION = Get-Prev-Stack-Version
-            # if (HasFleetServer("$OLD_STACK_VERSION")) {
-            #     $body=(@{'revoke' = $true} | ConvertTo-Json)
-            # }
-            # $jsonResult = ''
-            # if ( $powershellVersion -gt 3 ) {
-            #     $headers.Add("Accept","application/json")
-            # }
-            # try {
-            #     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-            #     $jsonResult = Invoke-WebRequest -Uri "$( $kibanaUrl )/api/fleet/agents/$( $agentId )/unenroll" -Body $body  -Method 'POST' -Headers $headers -UseBasicParsing -ContentType 'application/json; charset=utf-8'
-            # } catch {
-            #     $jsonResult = ConvertFrom-Json $result.ErrorDetails.Message  | Select-Object
-            # }
-            # if ($jsonResult.statuscode -eq '200')
-            # {
-            #     Write-Log "Unenrollment succeeded" "INFO"
-            # }
-            # elseif ($jsonResult.statuscode -eq '404' -And $jsonResult.error -eq "Not Found") {
-            #     Write-Log "Elastic agent was previously unenrolled" "INFO"
-            # }
-            # else {
-            #     throw "Unenrolling the agent failed, api request returned status $jsonResult.statuscode"
-            # }
-            # Write-Status "$name" "$firstOperation" "transitioning" "$message" "$subName" "success" "Elastic Agent service has been unenrolled"
-
             Write-Log "Uninstalling Elastic Agent" "INFO"
             & "$INSTALL_LOCATION\Elastic\Agent\elastic-agent.exe" uninstall --force
             Write-Log "Elastic Agent has been uninstalled" "INFO"
@@ -102,7 +37,6 @@ function Uninstall-Old-ElasticAgent {
                 Write-Log $_ "ERROR"
                 Write-Log $_.ScriptStackTrace "ERROR"
                 Write-Status "$name" "$operation" "error" "$message" "$subName" "error" "Elastic Agent service has been uninstalled"
-                # exit 1
                 Clean-And-Exit 1
             } else {
                 Write-Log "Elastic Agent installation failed. retrying in 20s" "ERROR"
